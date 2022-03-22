34c89a74c653cd355fbf3b7b6f48c35108b56db3
illupovero <davideluporini1996@gmail.com>
2022-03-22 19:26:59 +0100

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,8 @@
+<!-- Image and text -->
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Scudetto.svg/1200px-Scudetto.svg.png" width="30" height="30" class="d-inline-block align-top mx-3" alt="">
+      Bootstrap
+      {{-- INSERIRE NOME DINAMICO --}}
+    </a>
+  </nav>
